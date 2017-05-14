47396524a4a66985882df7ec8c008ce748b174fa
AveryPratt <apratt91@gmail.com>
2017-05-14 14:41:11 -0700

    Add project files.

---
+++ Orbitroids/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Orbitroids.MvcApplication" Language="C#" %>
---
+++ Orbitroids/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Orbitroids.Startup))]
+namespace Orbitroids
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
