295d02eca578a6db16f71317104c04564f3875bf
AveryPratt <apratt91@gmail.com>
2017-06-03 23:31:32 -0700

    changed red to random color

--- Orbitroids/Scripts/SignalR.js
+++ Orbitroids/Scripts/SignalR.js
@@ -3,7 +3,12 @@
 $(document).ready(function () {
     var game = $.connection.gameHub;
     game.client.update = function () {
-        $('canvas').css('background-color', 'red');
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        $('canvas').css('background-color', color);
     };
     $(document).keydown(function () {
         game.server.submitInput();
