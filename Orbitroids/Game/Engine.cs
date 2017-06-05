aca4281688dc99e6fdd6cb3193569166aabf6741
AveryPratt <apratt91@gmail.com>
2017-06-05 00:58:45 -0700

    Created planet and ship objects, and modified Engine to format vectors better.

--- Orbitroids/Game/Engine.cs
+++ Orbitroids/Game/Engine.cs
@@ -7,9 +7,9 @@ namespace Orbitroids.Game
 {
     public class Engine
     {
-        public class Point
+        public class Coordinate
         {
-            public Point(double x = 0, double y = 0)
+            public Coordinate(double x = 0, double y = 0)
             {
                 this.X = x;
                 this.Y = y;
@@ -27,16 +27,25 @@ namespace Orbitroids.Game
                 this.DeltaRot = deltaRot;
             }
 
-            public double ForwardAngle { get; set; }
+            public double ForwardAngle {
+                get
+                {
+                    return this.ForwardAngle;
+                }
+                set
+                {
+                    this.ForwardAngle = value;
+                    this.RefineForwardAngle();
+                }
+            }
             public double DeltaRot { get; set; }
 
             public void Rotate(double accelRot = 0)
             {
                 this.DeltaRot += accelRot;
                 this.ForwardAngle += DeltaRot;
-                this.RefineForwardAngle();
             }
-            protected void RefineForwardAngle()
+            protected internal void RefineForwardAngle()
             {
                 while (this.ForwardAngle >= Math.PI * 2)
                 {
@@ -53,69 +62,67 @@ namespace Orbitroids.Game
         {
             public Vector()
             {
-                this.Delta = new Point(0, 0);
-                this.Head = new Point(0, 0);
-                this.Origin = new Point(0, 0);
+                this.Delta = new Coordinate(0, 0);
+                this.Head = new Coordinate(0, 0);
+                this.Origin = new Coordinate(0, 0);
                 this.ForwardAngle = 0;
                 this.DeltaRot = 0;
                 this.Length = 0;
             }
 
-            public Point Origin { get; set; }
-            public Point Head { get; set; }
-            public Point Delta { get; set; }
+            public Coordinate Origin { get; set; }
+            public Coordinate Head { get; set; }
+            public Coordinate Delta { get; set; }
             public double Length { get; set; }
-            
-            public static Vector VecCart(Point head, Point origin = null, double deltaRot = 0)
-            {
-                var vector = new Vector();
-                vector.Head = head;
-                vector.Origin = origin ?? new Point(0, 0);
-                vector.DeltaRot = deltaRot;
-
-                vector.Delta = new Point(head.X - origin.X, head.Y - origin.Y);
-                vector.Length = Math.Sqrt(Math.Pow(vector.Delta.X, 2) + Math.Pow(vector.Delta.Y, 2));
-                var unitDelta = new Point(vector.Delta.X / vector.Length, vector.Delta.Y / vector.Length);
-                vector.ForwardAngle = unitDelta.Y < 0 ? Math.PI - Math.Asin(unitDelta.X) : Math.Asin(unitDelta.X);
-                vector.RefineForwardAngle();
-
-                return vector;
-            }
-            public static Vector VecDelta(Point delta, Point origin = null, double deltaRot = 0)
-            {
-                var vector = new Vector();
-                vector.Delta = delta;
-                vector.Origin = origin ?? new Point(0, 0);
-                vector.DeltaRot = deltaRot;
-
-                vector.Head = new Point(origin.X + delta.X, origin.Y + delta.Y);
-                vector.Length = Math.Sqrt(Math.Pow(delta.X, 2) + Math.Pow(delta.Y, 2));
-                var unitDelta = new Point(delta.X / vector.Length, delta.Y / vector.Length);
-                vector.ForwardAngle = unitDelta.Y < 0 ? Math.PI - Math.Asin(unitDelta.X) : Math.Asin(unitDelta.X);
-                vector.RefineForwardAngle();
-
-                return vector;
-            }
-            public static Vector VecCirc(double forwardAngle, double length, Point origin = null, double deltaRot = 0)
-            {
-                var vector = new Vector();
-                vector.ForwardAngle = forwardAngle;
-                vector.Length = length;
-                vector.Origin = origin ?? new Point(0, 0);
-                vector.DeltaRot = deltaRot;
-
-                vector.Delta = new Point(length * Math.Sin(forwardAngle), length * Math.Cos(forwardAngle));
-                vector.Head = new Point(origin.X + vector.Delta.X, origin.Y + vector.Delta.Y);
-
-                return vector;
-            }
-            public static Vector AddVectors(Vector originalVec, Vector deltaVec)
-            {
-                var delta = new Point(originalVec.Delta.X + deltaVec.Delta.X, originalVec.Delta.Y + deltaVec.Delta.Y);
-                return VecDelta(delta, originalVec.Origin);
-            }
         }
-        
+
+        public static Vector VecCart(Coordinate head, Coordinate origin = null, double deltaRot = 0)
+        {
+            var vector = new Vector();
+            vector.Head = head;
+            vector.Origin = origin ?? new Coordinate(0, 0);
+            vector.DeltaRot = deltaRot;
+
+            vector.Delta = new Coordinate(head.X - origin.X, head.Y - origin.Y);
+            vector.Length = Math.Sqrt(Math.Pow(vector.Delta.X, 2) + Math.Pow(vector.Delta.Y, 2));
+            var unitDelta = new Coordinate(vector.Delta.X / vector.Length, vector.Delta.Y / vector.Length);
+            vector.ForwardAngle = unitDelta.Y < 0 ? Math.PI - Math.Asin(unitDelta.X) : Math.Asin(unitDelta.X);
+
+            return vector;
+        }
+        public static Vector VecDelta(Coordinate delta, Coordinate origin = null, double deltaRot = 0)
+        {
+            var vector = new Vector();
+            vector.Delta = delta;
+            vector.Origin = origin ?? new Coordinate(0, 0);
+            vector.DeltaRot = deltaRot;
+
+            vector.Head = new Coordinate(origin.X + delta.X, origin.Y + delta.Y);
+            vector.Length = Math.Sqrt(Math.Pow(delta.X, 2) + Math.Pow(delta.Y, 2));
+            var unitDelta = new Coordinate(delta.X / vector.Length, delta.Y / vector.Length);
+            vector.ForwardAngle = unitDelta.Y < 0 ? Math.PI - Math.Asin(unitDelta.X) : Math.Asin(unitDelta.X);
+
+            return vector;
+        }
+        public static Vector VecCirc(double forwardAngle = 0, double length = 0, Coordinate origin = null, double deltaRot = 0)
+        {
+            var vector = new Vector();
+            vector.ForwardAngle = forwardAngle;
+            vector.Length = length;
+            vector.Origin = origin ?? new Coordinate(0, 0);
+            vector.DeltaRot = deltaRot;
+
+            vector.Delta = new Coordinate(length * Math.Sin(forwardAngle), length * Math.Cos(forwardAngle));
+            vector.Head = new Coordinate(origin.X + vector.Delta.X, origin.Y + vector.Delta.Y);
+
+            return vector;
+        }
+        public static Vector AddVectors(Vector originalVec, Vector deltaVec)
+        {
+            var delta = new Coordinate(originalVec.Delta.X + deltaVec.Delta.X, originalVec.Delta.Y + deltaVec.Delta.Y);
+            return VecDelta(delta, originalVec.Origin);
+        }
+
         public class Orbital : Rotational
         {
             public Orbital(Vector vel = null, Vector accel = null, double forwardAngle = 0, double deltaRot = 0)
@@ -133,20 +140,20 @@ namespace Orbitroids.Game
             //{
 
             //}
-            public void applyAccel(Vector accel)
+            public void ApplyAccel(Vector accel)
             {
-                this.Accel = Vector.AddVectors(this.Accel, accel);
+                this.Accel = AddVectors(this.Accel, accel);
             }
-            public void resetAccel()
+            public void ResetAccel()
             {
                 this.Accel = new Vector();
             }
-            public void applyMotion()
+            public void ApplyMotion()
             {
                 // converts acceleration to velocity
-                this.Vel = Vector.AddVectors(this.Vel, this.Accel);
+                this.Vel = AddVectors(this.Vel, this.Accel);
                 // converts velocity to distance
-                this.Vel = Vector.VecDelta(this.Vel.Delta, this.Vel.Head, this.Vel.DeltaRot);
+                this.Vel = VecDelta(this.Vel.Delta, this.Vel.Head, this.Vel.DeltaRot);
             }
         }
     }
--- Orbitroids/Game/Objects.cs
+++ Orbitroids/Game/Objects.cs
@@ -1,11 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
+using static Orbitroids.Game.Engine;
 
 namespace Orbitroids.Game
 {
     public class Objects
     {
+        public const double unit = 1;
+
+        public class Planet : Orbital
+        {
+            public Planet(int mass, int radius, Vector vel = null, Color color = new Color(), bool atmosphere = false, double forwardAngle = 0, double deltaRot = 0)
+            {
+                this.Mass = mass;
+                this.Radius = radius;
+                this.Vel = vel ?? new Vector();
+                this.Color = color;
+                this.Atmosphere = atmosphere;
+                this.ForwardAngle = forwardAngle;
+                this.DeltaRot = deltaRot;
+            }
+
+            public int Mass { get; set; }
+            public int Radius { get; set; }
+            public Color Color { get; set; }
+            public bool Atmosphere { get; set; }
+        }
+
+        public class Ship : Orbital
+        {
+            public Ship(Vector vel, Color color = new Color(), double forwardAngle = 0, double deltaRot = 0)
+            {
+                this.Vel = vel;
+                this.Color = color;
+
+                this.Burning = false;
+                this.DampenRot = false;
+                this.DampenBurn = false;
+                this.Loaded = false;
+                this.Exploded = false;
+                this.Destroyed = false;
+                this.TrueAnomaly = new Vector();
+                this.Accel = VecCirc();
+
+                this.alignPoints();
+            }
+
+            public Color Color { get; set; }
+            public bool Burning { get; set; }
+            public bool DampenRot { get; set; }
+            public bool DampenBurn { get; set; }
+            public bool Loaded { get; set; }
+            public bool Exploded { get; set; }
+            public bool Destroyed { get; set; }
+            public Coordinate Nose { get; set; }
+            public Coordinate LeftSide { get; set; }
+            public Coordinate RightSide { get; set; }
+            public Coordinate Rear { get; set; }
+            public Vector TrueAnomaly { get; set; }
+
+            private void alignPoints()
+            {
+                this.Nose = VecCirc(this.ForwardAngle, 10 * unit, this.Vel.Origin).Head;
+                this.LeftSide = VecCirc(this.ForwardAngle + 5 * Math.PI / 6, 10 * unit, this.Vel.Origin).Head;
+                this.RightSide = VecCirc(this.ForwardAngle - 5 * Math.PI / 6, 10 * unit, this.Vel.Origin).Head;
+                this.Rear = VecCirc(this.ForwardAngle - Math.PI, 10 * unit, this.Vel.Origin).Head;
+            }
+            new public void Rotate(double accelRot = 0)
+            {
+                if ((accelRot < 0 && this.DeltaRot > -.2) || 
+                    (accelRot > 0 && this.DeltaRot < .2))
+                {
+                    this.DeltaRot += accelRot;
+                }
+                this.ForwardAngle += this.DeltaRot;
+            }
+            new public void ApplyMotion()
+            {
+                base.ApplyMotion();
+                this.TrueAnomaly = VecCart(this.Vel.Origin, new Coordinate());
+                this.alignPoints();
+            }
+            public void Burn(double force)
+            {
+                this.Accel = AddVectors(this.Accel, VecCirc(this.ForwardAngle, force));
+            }
+            public void Shoot()
+            {
+                this.Accel = AddVectors(this.Accel, VecCirc(this.ForwardAngle - Math.PI, .5 * unit));
+                var projection = VecCirc(this.ForwardAngle, 2.5 * unit, this.Nose);
+                projection = AddVectors(projection, this.Vel);
+                new Shot(projection);
+            }
+        }
     }
 }
